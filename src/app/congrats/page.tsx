0486f58d6d7452221652ebff6c368bc161b88c5a
Erick <erick.kuwahara79@gmail.com>
2023-11-11 20:00:23 -0300

    feat: congrats page

--- src/app/congrats/page.tsx
+++ src/app/congrats/page.tsx
@@ -1,3 +1,33 @@
+"use client";
+
+import Image from "next/image";
+
+import { Button } from "@/components/button";
+import { useRouter } from "next/navigation";
+
 export default function Congrats() {
-  return <div>Congratulations</div>;
+  const router = useRouter();
+
+  function exit() {
+    router.push("/");
+  }
+
+  return (
+    <main className="flex flex-col justify-center items-center min-h-screen px-6">
+      <Image src={"/logo.svg"} alt="logo" height={70.64} width={105.25} priority />
+
+      <div className="flex flex-col items-center gap-9 max-w-[340px] w-full mt-10 mb-5">
+        <div className="flex flex-col gap-2">
+          <h1 className="text-3xl text-white">Congratulations</h1>
+          <p className="text-center text-[#A1A1A1]">You login is complete.</p>
+        </div>
+
+        <span className="text-xl text-white font-medium">That it&apos;s! 😃</span>
+
+        <Button variant="primary" onClick={exit} additionalClass="w-full max-w-[170px]">
+          Exit
+        </Button>
+      </div>
+    </main>
+  );
 }
--- src/components/button.tsx
+++ src/components/button.tsx
@@ -3,16 +3,18 @@ import { ComponentProps } from "react";
 
 interface ButtonProps extends ComponentProps<"button"> {
   variant: "primary" | "secondary";
+  additionalClass?: string;
 }
 
-export function Button({ variant, ...props }: ButtonProps) {
+export function Button({ variant, additionalClass, ...props }: ButtonProps) {
   return (
     <button
       className={clsx(
         "flex justify-center items-center gap-4 h-12 rounded-lg outline-none select-none transition-all",
         "text-white",
         { "bg-[#005EF3] hover:bg-[#004bc4] focus-within:bg-[#004bc4]": variant === "primary" },
-        { "border border-white hover:bg-white/10 focus-within:bg-white/10": variant === "secondary" }
+        { "border border-white hover:bg-white/10 focus-within:bg-white/10": variant === "secondary" },
+        additionalClass
       )}
       {...props}
     >
